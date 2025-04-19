de16fa3acfeae0d757f40810841605c171a43dac
vglx <xing.virgil@gmail.com>
2025-04-19 14:02:53 +1000

    Updating

--- include/Evaluation.h
+++ include/Evaluation.h
@@ -10,19 +10,9 @@ public:
                                const std::vector<Eigen::Matrix4d>& initial_poses,
                                const std::vector<Eigen::Matrix4d>& optimized_poses);
 
-       static void ComputeEulerRMSE(const std::vector<Eigen::Matrix4d>& ground_truth,
-                                    const std::vector<Eigen::Matrix4d>& initial_poses,
-                                    const std::vector<Eigen::Matrix4d>& optimized_poses);
-
 private:
        static void ComputeRMSE(const std::vector<Eigen::Matrix4d>& gt, 
-                               const std::vector<Eigen::Matrix4d>& poses);
-
-       static Eigen::Vector3d RotationMatrixToEulerZYX(const Eigen::Matrix3d& R);
-
-       static void ComputeEulerRMSE(const std::vector<Eigen::Matrix4d>& gt,
-                                    const std::vector<Eigen::Matrix4d>& poses,
-                                    const std::string& label);
+                                 const std::vector<Eigen::Matrix4d>& poses);
 };
 
 #endif // EVALUATION_H
